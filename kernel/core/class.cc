54a3d2fdf08a84849c9c525350906c3ad93ae43f
Zak <zakdoesgaming@gmail.com>
2015-05-20 13:09:54 +0100

    Added comments to explain code better

--- kernel/core/class.cc
+++ kernel/core/class.cc
@@ -2,10 +2,10 @@
 #include <os.h>
 
 
-Io 				io;			/* Input/Output interface */
-Architecture 	arch;		/*	Cpu and architecture interface */
-Vmm 			vmm;		/*	Virtual memory manager interface */
-Filesystem		fsm;		/*	Filesystem interface */
-Module			modm;		/* Module manager */
-Syscalls		syscall;	/* Syscalls manager */
-System			sys;		/* System manager */
+Io 				io;			// IO interface
+Architecture 	arch;		// CPU and Architecture interface
+Vmm 			vmm;		// Virtual memory manager
+Filesystem		fsm;		// Filesystem interface
+Module			modm;		// Module manager
+Syscalls		syscall;	// Syscalls interface
+System			sys;		// System manager
