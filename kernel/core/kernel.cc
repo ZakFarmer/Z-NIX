93fec55682b24065370d5bfdc6e1e74f789c072e
Zak <zakdoesgaming@gmail.com>
2015-05-20 13:01:01 +0100

    Added comments to explain code better

--- kernel/core/kernel.cc
+++ kernel/core/kernel.cc
@@ -1,12 +1,15 @@
+// Z-NIX Kernel
+// Created by Zak Farmer
 
+// Include core header files
 #include <os.h>
 #include <boot.h>
 
 
 static char* init_argv[2]={"init","-i"};
 
+// Load core system modules function
 static void load_modules(multiboot_info* mbi){
-	
 	if (mbi->mods_count>0){
 		u32 initrd_location = *((u32*)mbi->mods_addr);
 		u32 initrd_end = *(u32*)(mbi->mods_addr+4);
@@ -25,31 +28,38 @@ static void load_modules(multiboot_info* mbi){
 
 extern "C" void kmain(multiboot_info* mbi){
 	io.clear();
+	// Print kernel information
 	io.print("%s - %s -- %s %s \n",	KERNEL_NAME,
 									KERNEL_VERSION,
 									KERNEL_DATE,
 									KERNEL_TIME);
 	
+	// Print kernel licence (not needed yet, not yet defined)
 	io.print("%s \n",KERNEL_LICENCE);
 	arch.init();
 	
+	// Load VMem management
 	io.print("Loading Virtual Memory Management... \n");
 	vmm.init(mbi->high_mem);
 	
+	// Load FS management
 	io.print("Loading FileSystem Management... \n");
 	fsm.init();
 	
+	// Load syscalls interface
 	io.print("Loading syscalls interface... \n");
 	syscall.init();
 	
+	// Load system core
 	io.print("Loading system... \n");
 	sys.init();
 	
+	// Load system modules
 	io.print("Loading modules... \n");
 	modm.init();
 	modm.initLink();
 
-
+	// Mount disks to filesystem
 	modm.install("hda0","module.dospartition",0,"/dev/hda");
 	modm.install("hda1","module.dospartition",1,"/dev/hda");
 	modm.install("hda2","module.dospartition",2,"/dev/hda");
@@ -58,16 +68,19 @@ extern "C" void kmain(multiboot_info* mbi){
 
 	arch.initProc();
 	
+	// Load extra binary modules for system operation
 	io.print("Loading binary modules \n");
 	load_modules(mbi);
 	
 	fsm.link("/mnt/boot/bin/","/bin/");
 
-	
+	// Print ready message (if system initialised correctly)
 	io.print("\n");
 	io.print("==== Z-NIX ==== System is ready (%s - %s) ==== \n",KERNEL_DATE,KERNEL_TIME);
+	// Enable interrupts to handle errors nicely without damaging the OS or system itself
 	arch.enable_interrupt();
 	for (;;);
+	// If system is halted before shutdown command is run, run shutdown on the kernel before damage is done
 	arch.shutdown();
 }
 
