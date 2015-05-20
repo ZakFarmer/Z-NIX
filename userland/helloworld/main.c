1f05682be7878dcd53e8bb3ff7cd96ccb43727a5
zakdoesgaming <zakdoesgaming@gmail.com>
2015-05-20 21:12:31 +0100

    Added input into test helloworld program that runs at the OS boot. Input currently doesn't work as the program does not have permission to write to the memory address.

--- userland/helloworld/main.c
+++ userland/helloworld/main.c
@@ -7,6 +7,10 @@
 #include <unistd.h>
 
 int main(int argc,char **argv){
+	int number;
+
 	printf("Z-NIX booted, hello world! \n");
+	printf("What is your favourite number? ");
+	scanf("%s", &number);
 	return 0;
 }
