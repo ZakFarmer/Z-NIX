b3f3eb918d935475a22f5b604523af8feedb9867
Zak <zakdoesgaming@gmail.com>
2015-05-22 13:04:25 +0100

    Added comments to explain code better

--- kernel/core/device.cc
+++ kernel/core/device.cc
@@ -13,30 +13,37 @@ Device::Device(char* n) : File(n,TYPE_DEVICE)
 	fsm.addFile("/dev",this);
 }
 
+// Open connection to device
 u32	Device::open(u32 flag){
 	return NOT_DEFINED;
 }
 
+// Close connection to device
 u32	Device::close(){
 	return NOT_DEFINED;
 }
 
+// Read from a device connection
 u32	Device::read(u8* buffer,u32 size){
 	return NOT_DEFINED;
 }
 
+// Write to a device connection
 u32	Device::write(u8* buffer,u32 size){
 	return NOT_DEFINED;
 }
 
+// Initialise IO to device connection
 u32	Device::ioctl(u32 id,u8* buffer){
 	return NOT_DEFINED;
 }
 
+// Remove device connection
 u32	Device::remove(){
 	delete this;
 }
 
+// Scan for devices
 void Device::scan(){
 
 }
